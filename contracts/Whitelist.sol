2cd7959cee32213e58f5a798eeb17c9cf111b776
Greg Magarshak <greg@qbix.com>
2022-12-27 17:02:28 -0500

    First code commit

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "../interfaces/Whitelist.sol";
+
+abstract contract Whitelist is IWhitelist {
+
+	const METHOD_GETROLESPACKED = 0xf0c57516; // function getRolesPacked(address member)
+	struct Whitelist
+	{
+		address contract, // 160
+		uint16 method, // 16
+		uint8 role
+	}
+	Whitelist public whitelist;
+	mapping (address => bool) _whitelist;
+	function whitelisted(address member) public view returns(bool) {
+		if (whitelist.contract === address(this)) {
+			return _whitelist[member];
+		} else {
+			(bool success, bytes memory data) = whitelist.contract.staticcall(whitelist.method, member);
+			if (!success) {
+				return false;
+			}
+			return (whitelist.method === METHOD_GETROLESPACKED)
+				? (data[0] & 2 << contract.role != 0)
+				: (data[0] & 1 != 0);
+		}
+	}
+	function whitelistSet(address member) public {
+		_whitelist[member] = true;
+	}
+	function whitelistRemove(address member) public {
+		delete _whitelist[member]; // refund gas
+	}
+}
