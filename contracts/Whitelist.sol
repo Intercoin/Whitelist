0ceb18b5f623092cb3637f5c44cae3f6411195c4
Greg Magarshak <greg@qbix.com>
2022-12-27 19:48:57 -0500

    Tested and fixed Whitelist

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,33 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.11;
+pragma solidity 0.8.10;
 
-import "../interfaces/Whitelist.sol";
+import "../interfaces/IWhitelist.sol";
 
 abstract contract Whitelist is IWhitelist {
 
-	const METHOD_GETROLESPACKED = 0xf0c57516; // function getRolesPacked(address member)
+	bytes4 public constant METHOD_GETROLESPACKED = 0xf0c57516; // function getRolesPacked(address member)
 	struct Whitelist
 	{
-		address contract, // 160
-		uint16 method, // 16
-		uint8 role
+		address contractAddress; // 160
+		bytes4 method; // 32
+		uint8 role; // 8
 	}
 	Whitelist public whitelist;
 	mapping (address => bool) _whitelist;
 	function whitelisted(address member) public view returns(bool) {
-		if (whitelist.contract === address(this)) {
+		if (whitelist.contractAddress == address(this)) {
 			return _whitelist[member];
-		} else {
-			(bool success, bytes memory data) = whitelist.contract.staticcall(whitelist.method, member);
-			if (!success) {
-				return false;
-			}
-			return (whitelist.method === METHOD_GETROLESPACKED)
-				? (data[0] & 2 << contract.role != 0)
-				: (data[0] & 1 != 0);
 		}
+		(bool success, bytes memory data) = whitelist.contractAddress.staticcall(abi.encodeWithSelector(whitelist.method, member));
+		if (!success) {
+			return false;
+		}
+		if (whitelist.method != METHOD_GETROLESPACKED) {
+			return abi.decode(data, (bool));
+		}
+		uint256 roles = abi.decode(data, (uint256));
+		return (roles & (1 << whitelist.role)) != 0;
 	}
-	function whitelistSet(address member) public {
+    function whitelisted2(address member) public view returns(uint256) {
+		(bool success, bytes memory data) = whitelist.contractAddress.staticcall(abi.encodeWithSelector(whitelist.method, member));
+		uint256 roles = abi.decode(data, (uint256));
+		return roles;
+	}
+	function whitelistAdd(address member) public {
 		_whitelist[member] = true;
 	}
 	function whitelistRemove(address member) public {
--- interfaces/IWhitelist.sol
+++ interfaces/IWhitelist.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.11;
+pragma solidity 0.8.10;
 
-interface ISafeHook {
-    function whitelisted(address member) public view returns(bool);
+interface IWhitelist {
+    function whitelisted(address member) external view returns(bool);
 }
