e5c6c83971faf95139573ee08084cfb46ab39f78
Artem Subbotin <subbotin.artem@gmail.com>
2023-01-13 02:39:33 +0200

    files structure redesign

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import "../interfaces/IWhitelist.sol";
+import "./interfaces/IWhitelist.sol";
 
 abstract contract Whitelist is IWhitelist {
 
