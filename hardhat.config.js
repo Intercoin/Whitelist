0049a3eb352e8bc5cb5e3aba4dac0e4f49cbc40c
Artem Subbotin <subbotin.artem@gmail.com>
2023-11-24 17:25:42 +0200

    renamed to `@intercoin/whitelist`. forces fixed vulnerabilities

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,12 @@
 require('dotenv').config()
-
-require("@nomiclabs/hardhat-ethers")
 require('hardhat-docgen')
 require('hardhat-deploy')
-require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-web3")
-require("@nomiclabs/hardhat-etherscan")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-//require("hardhat-docgen")
 require("@hardhat-docgen/core")
-//require("@hardhat-docgen/markdown")
-//require("./docgen-custom-markdown")
-
+require('hardhat-contract-sizer')
+require("hardhat-gas-trackooor");
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
 
 const kovanURL = `https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMY_KOVAN}`
 const goerliURL = `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI}`
@@ -60,7 +54,7 @@ module.exports = {
       accounts: [process.env.private_key],
       saveDeployments: true
     },
-    matic: {
+    polygon: {
       url: maticURL,
       chainId: 137,
       //gasPrice: "auto",
